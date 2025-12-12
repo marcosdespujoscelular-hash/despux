6fc7420c6454fa04b2f0f34419b5b02cc0813dfd
marcosdespujoscelular-hash <marcosdespujoscelular@gmail.com>
2025-12-11 22:57:45 -0400

    feat: Ruta base activada y nueva dist

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, '.', '');
   return {
-
+    // ESTA ES LA LÍNEA CLAVE CORREGIDA PARA GITHUB PAGES
     base: '/despux/', 
     
     server: {
